96ca61fdc201229bba88843b0c384b7d70664709
“Versua0” <1617626942@qq.com>
2023-03-14 08:52:52 +0800

    添加了拦截器

--- src/main/java/com/common/ctdenoising/config/WebMvcConfig.java
+++ src/main/java/com/common/ctdenoising/config/WebMvcConfig.java
@@ -1,15 +1,29 @@
 package com.common.ctdenoising.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .maxAge(3600)
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(CommonInterceptor()).addPathPatterns("/**");
+    }
+    @Bean
+    public CommonInterceptor CommonInterceptor(){
+        return new CommonInterceptor();
     }
 }
